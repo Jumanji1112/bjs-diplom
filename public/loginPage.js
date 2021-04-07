1dfc961409c1e9a2450d14d0396e41357e9ea1f5
Alisa A <Jumanji1112@mail.ru>
2021-04-07 19:50:32 +0300

    Вход и регистрация

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,6 +1,19 @@
 'use strict'
 const userForm = new UserForm();
+userForm.loginFormCallback = (data) =>  ApiConnector.login (data, response => {
+	if (response.success == false) {
+	 userForm.setLoginErrorMessage(response.error); 
+	}
+	location.reload();
+});
 
-userForm.loginFormCallback = (data) => {
-	ApiConnector.login(data, loginFormCallback ());
-}
+
+userForm.registerFormCallback = (data) => ApiConnector.register (data, response => {   
+	if ( response.success == true) {
+		location.reload();
+	} else {
+		userForm.setRegisterErrorMessage(response.error);
+	}
+});
+
+
